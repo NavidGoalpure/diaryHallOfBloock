abdf393bee10e09741dd53c98fe8361e0251c3b8
Navid Goalpure <Navid.goalpure@gmail.com>
2019-05-03 13:36:26 +0430

    Updates

---
+++ precache-manifest.5f86ea12cb081de667cf3f3dcdddf87d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c44b2395749e8cc6784",
+    "url": "/static/css/main.96735265.chunk.css"
+  },
+  {
+    "revision": "3c44b2395749e8cc6784",
+    "url": "/static/js/main.de07f35a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4df85af44ac92c3470ad",
+    "url": "/static/js/2.c5b53772.chunk.js"
+  },
+  {
+    "revision": "331c39315fd661b27d8bba271fdb3fc0",
+    "url": "/static/media/Vazir.331c3931.woff"
+  },
+  {
+    "revision": "00dac7548bf7493c9042ad817d34cd46",
+    "url": "/static/media/Vazir.00dac754.eot"
+  },
+  {
+    "revision": "398b39dd0060814801cd1cbfe43fe0b5",
+    "url": "/static/media/Vazir.398b39dd.ttf"
+  },
+  {
+    "revision": "de557db116bb5cbec58ed123daf49c2d",
+    "url": "/static/media/bg.de557db1.jpg"
+  },
+  {
+    "revision": "d9e430d25019bf9f5ada52b5525fb02b",
+    "url": "/index.html"
+  }
+];
